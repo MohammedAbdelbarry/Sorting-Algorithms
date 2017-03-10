32927a4930f3a15257e7197b3963127484bf2ae1
Mohammed Abdelbarry <m7md337@gmail.com>
2017-03-10 23:02:25 +0200

    Modified docs

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['insert',['insert',['../class_heap.html#acdf7a3816b25f9f405d4771b06810dc7',1,'Heap::insert()'],['../class_heap_impl.html#a7a4cd1adff4a90b97fa57d9e3473963a',1,'HeapImpl::insert()']]],
   ['insertionsort',['insertionSort',['../namespacesort.html#ac2bd40f43951c00bffc1f41794d6d2ef',1,'sort::insertionSort(RandomAccessIterator first, RandomAccessIterator last, Comparator comparator)'],['../namespacesort.html#a68786433056001c68879cc2d20ea6bb7',1,'sort::insertionSort(RandomAccessIterator first, RandomAccessIterator last)']]],
+  ['introsort',['introSort',['../namespacesort.html#abe7e05b478c4cc520be65188e13bceef',1,'sort::introSort(RandomAccessIterator first, RandomAccessIterator last, Comparator comparator, int remRecursionDepth)'],['../namespacesort.html#ab9b66d661d74f8a2b3337b4aafe8a4b0',1,'sort::introSort(RandomAccessIterator first, RandomAccessIterator last)'],['../namespacesort.html#abbdb25eefda43c164a26b58c7e845798',1,'sort::introSort(RandomAccessIterator first, RandomAccessIterator last, Comparator comparator)']]],
   ['isempty',['isEmpty',['../class_heap.html#a2cba0489c655bd8169b3ad7a23873110',1,'Heap::isEmpty()'],['../class_heap_impl.html#a1bd295f5c7269253d4a33fad34f42e8b',1,'HeapImpl::isEmpty()'],['../class_priority_queue.html#a94988be14d1545d6f1f5c4d9bbdf5df2',1,'PriorityQueue::isEmpty()']]]
 ];
